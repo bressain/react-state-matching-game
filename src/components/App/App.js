9ceb94ad006e6b3901ca93afa67611b7cc929972
Bressain Dinkelman <dinkelburt@gmail.com>
2020-07-22 14:21:43 -0600

    Handle click events

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
-import { createTiles } from '../../misc/utils'
+import { createTiles, indexOfSelected } from '../../misc/utils'
 
 import './App.css';
 
@@ -21,8 +21,35 @@ class App extends Component{
       playing: true,
       previousTileIndex: null,
       toBeCleared: null,
-      tiles: createTiles(state.numTiles)
+      tiles: createTiles(state.numTiles, this.handleTileClicked)
     }))
+  }
+  handleTileClicked(id, color) {
+    this.setState(state => {
+      let { tiles, toBeCleared, previousTileIndex } = state
+      const selectedTileIndex = indexOfSelected(tiles, id, color)
+
+      if (toBeCleared !== null) {
+        tiles[toBeCleared[0]].selected = false
+        tiles[toBeCleared[1]].selected = false
+        toBeCleared = null
+      }
+      tiles[selectedTileIndex].selected = true
+      if (previousTileIndex !== null) {
+        const previousTile = tiles[previousTileIndex]
+        const selectedTile = tiles[selectedTileIndex]
+        if (previousTile.id !== selectedTile.id && previousTile.color === color) {
+          selectedTile.matched = true
+          previousTile.matched = true
+        } else {
+          toBeCleared = [previousTileIndex, selectedTileIndex]
+        }
+        previousTileIndex = null
+      } else {
+        previousTileIndex = selectedTileIndex
+      }
+      return { tiles, toBeCleared, previousTileIndex }
+    })
   }
   render() {
     return (
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -6,7 +6,7 @@ const Tile = props => {
   const selectedOrMatched = props.selected || props.matched
   const styles = selectedOrMatched ? { backgroundColor: props.color } : null
   return (
-    <div className='Tile' style={styles}>
+    <div className='Tile' style={styles} onClick={() => props.handleTileClicked(props.id, props.color)}>
       {selectedOrMatched ? <props.svg /> : null}
     </div>
   )
